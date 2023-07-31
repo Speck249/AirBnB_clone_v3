5c843e28dfc7fcea51ac98f17afc2e2e492ad6a0
Rediet <redietkfle@gmail.com>
2023-07-31 09:34:34 -0700

    Pycodestyle update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,25 +7,25 @@ from flask import jsonify
 from models import storage
 
 
-@app.route('/status')
+@app_views.route('/status')
 def status():
     """
     Method displays status.
     """
-    return jsonify('{status': 'OK'})
+    return jsonify({'status': 'OK'})
 
 
-@app.route('/stats')
+@app_views.route('/stats')
 def stats():
     """
     Method returns individual object count.
     """
     objects = {
-            "amenities": storage.count.('Amenity') 
-            "cities": storage.count.('City'),
-            "places": storage.count.('Place'),
-            "reviews": storage.count.('Review'),
-            "states": storage.count.('State'),
-            "users": storage.count.('User')
+            "amenities": storage.count('Amenity'), 
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')
             }
     return jsonify(objects)
